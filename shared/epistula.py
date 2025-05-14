c63672db1196e941d04bc22964ae62c6cfec29cd
bkb2135 <bkb2135@columbia.edu>
2025-05-14 11:02:20 +0000

    Multimodel Chat Format When Messages is None

--- shared/epistula.py
+++ shared/epistula.py
@@ -251,7 +251,8 @@ async def make_openai_query(
         ),
     )
     extra_body = {k: v for k, v in body.items() if k not in ["messages", "model"]}
-    body["messages"] = model_factory(body.get("model")).format_messages(body["messages"])
+    if extra_body["task"] == "InferenceTask" or body.get("model") is None:
+        body["messages"] = model_factory(body.get("model")).format_messages(body["messages"])
 
     start_time = time.perf_counter()
     chat = await miner.chat.completions.create(
