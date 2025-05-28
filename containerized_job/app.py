60a8f59ba8a75b9bbef15fbbef9b1ba00db94f41
chrisu-inigra <krzysztof.urbanowski@inigra.eu>
2025-05-28 10:42:16 +0200

    Fixes from pre-commit hooks

--- containerized_job/app.py
+++ containerized_job/app.py
@@ -1,9 +1,8 @@
 import os
 
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from loguru import logger
 from schema import ChatRequest, LogitsRequest
 from vllm_llm import ReproducibleVLLM
 
--- containerized_job/schema.py
+++ containerized_job/schema.py
@@ -1,6 +1,6 @@
 from typing import List, Literal, Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 
 class ChatMessage(BaseModel):
