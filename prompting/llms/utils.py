c28417851ee9a3712fcb966760d0e9c0dea8d0d0
Dmytro Bobrenko <17252809+dbobrenko@users.noreply.github.com>
2025-06-16 15:24:29 +0000

    More hints

--- prompting/llms/utils.py
+++ prompting/llms/utils.py
@@ -117,7 +117,7 @@ class GPUInfo:
         return cls.used_memory / cls.total_memory
 
 
-TEXT_MODELS = [None, "mrfakename/mistral-small-3.1-24b-instruct-2503-hf"]
+TEXT_MODELS: set[str | None] = set([None, "mrfakename/mistral-small-3.1-24b-instruct-2503-hf"])
 
 
 def model_factory(model_name: str) -> type[ReproducibleHF]:
