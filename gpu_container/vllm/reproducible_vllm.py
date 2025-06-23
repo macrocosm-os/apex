2d5d98b133134a46ec8ccf0bb01d1f062bd8073e
bkb2135 <98138173+bkb2135@users.noreply.github.com>
2025-06-23 15:13:55 +0100

    Update reproducible_vllm.py

--- gpu_container/vllm/reproducible_vllm.py
+++ gpu_container/vllm/reproducible_vllm.py
@@ -3,10 +3,7 @@ from typing import Dict, List, Optional, Union
 
 import numpy as np
 import torch
-from tqdm import tqdm
 from vllm import LLM, SamplingParams
-
-tqdm.disable = True
 
 
 class ReproducibleVLLM:
