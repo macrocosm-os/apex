1ec0b2689d0fbe6e7186be3bdc4408caa6479a3b
bkb2135 <bkb2135@columbia.edu>
2025-08-07 14:53:05 +0000

    Refactor type hints in generate_header function to use modern syntax with dict and list types.

--- apex/common/epistula.py
+++ apex/common/epistula.py
@@ -1,10 +1,10 @@
 import json
-from typing import Any, Dict, List, Union
+from typing import Any
 
 import bittensor as bt
 
 
 def generate_header(
-    hotkey: bt.wallet.hotkey, body: Union[Dict[Any, Any], List[Any]]
-) -> Dict[str, Any]:
+    hotkey: bt.wallet.hotkey, body: dict[Any, Any] | list[Any]
+) -> dict[str, Any]:
     return {"Body-Signature": "0x" + hotkey.sign(json.dumps(body)).hex()}
